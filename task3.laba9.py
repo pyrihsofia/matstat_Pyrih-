06ceeb729a1f3939242e06953bb78a4150e7d797
pyrihhhhh <pyrihsofia31@gmail.com>
2025-11-28 16:47:30 +0200

    Add task 3 laba 9

---
+++ task3.laba9.py
@@ -0,0 +1,12 @@
+s = input()
+
+if len(s) <= 3:
+    print(s)
+else:
+    parts = []
+    while len(s) > 3:
+        parts.append(s[-3:])
+        s = s[:-3]
+    parts.append(s)
+    parts.reverse()
+    print(" ".join(parts))
